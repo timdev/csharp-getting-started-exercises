c90d8dd999057bd1acaf9bdd3e7843900958cbeb
Tim Lieberman <tim@timdev.com>
2016-09-03 17:12:46 -0700

    Solution for Lesson 10

--- Program.cs
+++ Program.cs
@@ -5,9 +5,18 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        // Write a program that initializes a list integer numbers,
+        // and then prints the numbers out along with their sum. 
         public static void Main()
         {
-            
+            var nums = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+
+            Console.WriteLine("Numbers: " + String.Join(", ", nums));
+
+            int sum = 0;
+            foreach(int num in nums) sum += num;
+
+            Console.WriteLine($"Sum: {sum}");
         }
     }
 
